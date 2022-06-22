367b74bcf6db02f7322a98f4d3e05d68d56869b3
Maks9481 <maks9481702@gmail.com>
2022-06-22 21:09:28 +0300

    ntn

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const { Telegraf } = require('telegraf')
 // npm run dev
+const axios = require('axios')
 require('dotenv').config()//1
 //const text = require('./const')
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply(`Привіт ${ctx.message.from.first_name, 'Я чат-бот створенний щоб показувати курс валют на данний момент'}`))
 bot.help((ctx) => ctx.reply('Idk what is'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', async (ctx) => {
+   try {
+      const currencyObj = await axios.get('https://api.monobank.ua/bank/currency')
+      return ctx.reply(currencyObj.data[0]);
+   } catch (error) {
+      return ctx.reply(error)
+   }
+});
 bot.launch()
